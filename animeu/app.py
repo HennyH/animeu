3c103900fee1766d901a584acacd298406399acc
Gavin <gavinvietnguyen2710@gmail.com>
2019-04-17 18:02:31 +0800

    (ui) show gallery of character pictures

--- animeu/app.py
+++ animeu/app.py
@@ -31,7 +31,11 @@ DATA_JSON = json.loads(open(DATA_FILE, "rb").read(), encoding="utf8")
 @app.route("/")
 def index():
     """Render an example page."""
+    character1 = random.choice(DATA_JSON)
+    character1["pictures"]["gallery"] = [p for p in character1["pictures"]["gallery"] if "23x32" not in p]
+    character2 = random.choice(DATA_JSON)
+    character2["pictures"]["gallery"] = [p for p in character2["pictures"]["gallery"] if "23x32" not in p]
     return render_template('battle.html', **{
-        "left": random.choice(DATA_JSON),
-        "right": random.choice(DATA_JSON)
+        "left": character1,
+        "right": character2
     })
