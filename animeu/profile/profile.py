1c675f3bff3cf3da595e485c038287aeb68d33fe
Gavin <gavinvietnguyen2710@gmail.com>
2019-05-11 19:22:19 +0800

    (profile) display recent battles and layout improvements
    (css) put space before brace

--- animeu/profile/profile.py
+++ animeu/profile/profile.py
@@ -9,7 +9,8 @@ from flask import Blueprint, render_template, Response
 from flask_login import current_user, login_required
 
 from animeu.data_loader import get_character_by_name
-from animeu.feed.queries import query_most_winning_waifus
+from .queries import (get_favourite_waifu_list,
+                      get_favourited_waifu_battles)
 from .logic import (maybe_get_favourited_waifu,
                     favourite_a_waifu,
                     unfavourite_a_waifu)
@@ -21,18 +22,27 @@ profile_bp = Blueprint("profile_bp", __name__, template_folder="templates")
 @login_required
 def profile():
     """Return the users profile page."""
-    top_waifus = []
-    for result in query_most_winning_waifus():
-        character = get_character_by_name(result.name)
-        top_waifus.append({
+    best_waifus = []
+    for result in get_favourite_waifu_list(current_user.id):
+        character = get_character_by_name(result.character_name)
+        best_waifus.append({
             "en_name": character["names"]["en"][0],
             "jp_name": character["names"]["jp"][0],
-            "win_count": result.wins,
-            "loss_count": result.losses,
+            "date-favour": result.date.strftime("%d %b %Y"),
             "gallery": character["pictures"]["gallery"]
         })
+    recent_battles = get_favourited_waifu_battles(current_user.id)
+    battle_summaries = [
+        {
+            "date": b.date,
+            "winner": get_character_by_name(b.winner_name),
+            "loser": get_character_by_name(b.loser_name)
+        }
+        for b in recent_battles
+    ]
     return render_template("profile.html",
-                           top_waifus=top_waifus)
+                           best_waifus=best_waifus,
+                           battles=battle_summaries)
 
 @profile_bp.route("/favourite/<name>", methods=["POST"])
 @login_required
